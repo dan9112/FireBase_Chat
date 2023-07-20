35e2b72c6517bfc45da62d57128d7f7d095faf2e
Danil Bessonov <danbes1997@gmail.com>
2023-07-20 11:57:08 +0300

    добавил простейшую имитацию чата

--- app/src/main/java/ru/lord/firebase_chat/MainActivity.kt
+++ app/src/main/java/ru/lord/firebase_chat/MainActivity.kt
@@ -2,12 +2,15 @@ package ru.lord.firebase_chat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import ru.lord.firebase_chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +21,20 @@ class MainActivity : AppCompatActivity() {
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+        onChangeListener(dRef = myRef)
+    }
+
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) =
+                    binding.rcView.append("\nDanil: ${snapshot.value}")
+
+                override fun onCancelled(error: DatabaseError) {}
+            }
+        )
     }
 }
