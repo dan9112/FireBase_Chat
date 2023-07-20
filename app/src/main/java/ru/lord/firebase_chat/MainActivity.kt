34452f3e8bb38edd7e0a6ed9e478cd2f8afd4ca6
Danil Bessonov <danbes1997@gmail.com>
2023-07-20 11:17:22 +0300

    связал приложение с Firebase

--- app/src/main/java/ru/lord/firebase_chat/MainActivity.kt
+++ app/src/main/java/ru/lord/firebase_chat/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.lord.firebase_chat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import ru.lord.firebase_chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
